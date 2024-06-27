5a181cc3f10795ab880928e61b03b8e5d4ac94ca
elisteph <stephaney.gyaase@kintampo-hrc.org>
2024-06-27 14:45:07 +0200

    Update githubdesk.R
    Added some codes

--- githubdesk.R
+++ githubdesk.R
@@ -1,2 +1,21 @@
 ##### Testing how to load my files####
 hello 
+library(psych) ##For descriptive statistics
+library(dplyr)
+library(tableone)
+install.packages("rafalib")
+library(rafalib)
+install.packages("swirl")
+library(swirl)
+pr2<- read.table("//Users//sgyaase//Documents//Adance_Effect//DAY2//data_IV.txt")
+
+####Finding out all the names in data set
+names(pr2)
+#####Describing a variable (Gives more than the summarize command)
+describe(pr2$age)
+#####Creating Frequency Tables in R (One variable)
+tb1<-table(pr2$program)
+tb1
+#####Creating proportion tables
+tb1<-table(pr2$program) ###First create the frequency table 
+prop.table(tb1)
